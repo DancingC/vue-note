932265dc5b9a47b0259022d5b7dc729f0bb7eeb3
fengyang <787133298@qq.com>
2019-01-22 10:21:49 +0800

    第一次提交

---
+++ vue-note/src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <li @mouseover="showBtn" @mouseout="liveLi">
+      <el-checkbox label='' v-model="checked" name="type" id="checked">{{ title }}</el-checkbox>
+      <el-button type="danger" icon="el-icon-delete" class="icon-btn transition-box" v-show="show" circle v-on:click="$emit('remove')"></el-button>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['title'],
+  data () {
+    return {
+      checked: false,
+      show: false
+    }
+  },
+  watch: {
+    checked: function () {
+      this.$nextTick(function () {
+        if (this.checked === true) {
+          this.$el.childNodes[0].childNodes[1].classList.add('line')
+        } else {
+          this.$el.childNodes[0].childNodes[1].classList.remove('line')
+        }
+      })
+    }
+  },
+  methods: {
+    showBtn: function () {
+      this.show = true
+    },
+    liveLi: function () {
+      this.show = false
+    }
+  }
+}
+</script>
+
+<style>
+.icon-btn{
+    float: right;
+    padding: 8px !important;
+    /* display: none; */
+}
+.line{
+  text-decoration:line-through;
+  color:#ddd !important;
+}
+</style>
