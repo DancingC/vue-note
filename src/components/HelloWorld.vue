877764febeefcbcef18495d331ab4fcad6928d2f
fengyang <787133298@qq.com>
2019-03-25 17:53:40 +0800

    代码提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <form v-on:submit.prevent="addNewTodo">
       <label class="el-form-item__label" for="new-todo">新增待办事项：</label>
       <div class="el-input">
-      <input v-model="newTodoText" id="new-todo" placeholder="请输入">
+        <input v-model="newTodoText" id="new-todo" placeholder="请输入">
       </div>
       <button>添加</button>
     </form>
@@ -12,7 +12,7 @@
         is="todo-item"
         v-for="(todo, index) in todos"
         v-bind:key="todo.id"
-        v-bind:title="todo.title"
+        v-bind:title="todo.name"
         v-on:remove="todos.splice(index, 1)"
         class="el-dropdown-menu__item"
       ></li>
@@ -21,44 +21,60 @@
 </template>
 
 <script>
-import TodoItem from './TodoItem'
+import TodoItem from "./TodoItem";
+import axios from "axios";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {
     TodoItem
   },
-  data () {
+  data() {
     return {
-      newTodoText: '',
+      newTodoText: "",
       todos: [],
       nextTodoId: 1
-    }
+    };
+  },
+  created() {
+    var _this = this;
+    axios.get("http://localhost:3000/about").then(function(response) {
+      _this.todos = response.data;
+    });
   },
   watch: {
-    todos: function () {
-      this.$nextTick(function () {
-        console.log(233)
-      })
+    todos: function() {
+      this.$nextTick(function() {
+        // console.log(233)
+      });
     }
   },
   methods: {
-    addNewTodo: function () {
-      if (this.newTodoText === '') {
+    addNewTodo: function() {
+      if (this.newTodoText === "") {
         this.$message({
-          message: '请输入待办事项哦！',
-          type: 'warning'
-        })
+          message: "请输入待办事项哦！",
+          type: "warning"
+        });
       } else {
         this.todos.push({
           id: this.nextTodoId++,
-          title: this.newTodoText
-        })
-        this.newTodoText = ''
+          name: this.newTodoText
+        });
+        // this.newTodoText = ""
+
+        var abc = this.newTodoText
+        axios({
+          method: 'post',
+          url: 'http://localhost:3000/add',
+          data: {
+            name: abc
+          }
+        });
       }
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -78,55 +94,55 @@ a {
   color: #42b983;
 }
 
-.note ul{
+.note ul {
   width: 400px;
 }
-.note ul li{
+.note ul li {
   padding: 5px;
   border: 1px solid #ddd;
   margin-bottom: -1px;
 }
-.note .el-input{
+.note .el-input {
   width: 180px;
 }
-.note input{
+.note input {
   -webkit-appearance: none;
-    background-color: #fff;
-    background-image: none;
-    border-radius: 4px;
-    border: 1px solid #dcdfe6;
-    box-sizing: border-box;
-    color: #606266;
-    display: inline-block;
-    font-size: inherit;
-    height: 40px;
-    line-height: 40px;
-    outline: none;
-    padding: 0 15px;
-    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
-    width: 100%;
-}
-button{
+  background-color: #fff;
+  background-image: none;
+  border-radius: 4px;
+  border: 1px solid #dcdfe6;
+  box-sizing: border-box;
+  color: #606266;
   display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
-    cursor: pointer;
-    background: #409eff;
-    border: 1px solid #409eff;
-    border-color: #409eff;
-    color: #fff;
-    -webkit-appearance: none;
-    text-align: center;
-    box-sizing: border-box;
-    outline: none;
-    margin: 0;
-    transition: .1s;
-    font-weight: 500;
-    -moz-user-select: none;
-    -webkit-user-select: none;
-    -ms-user-select: none;
-    padding: 12px 20px;
-    font-size: 14px;
-    border-radius: 4px;
+  font-size: inherit;
+  height: 40px;
+  line-height: 40px;
+  outline: none;
+  padding: 0 15px;
+  transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+  width: 100%;
+}
+button {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #409eff;
+  border: 1px solid #409eff;
+  border-color: #409eff;
+  color: #fff;
+  -webkit-appearance: none;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  transition: 0.1s;
+  font-weight: 500;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
 }
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'TodoItem',
   props: ['title'],
